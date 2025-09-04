1251c43013b8911678c989a0052cfaaefc3edd91
skarihacks <skarivinna@gmail.com>
2025-09-04 09:48:22 +0000

    firsta commit

---
+++ src/main/java/is/hi/hbv501g/demo/Hugbunadarverkefni1Application.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hugbunadarverkefni1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hugbunadarverkefni1Application.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/demo/Hugbunadarverkefni1ApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hugbunadarverkefni1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
