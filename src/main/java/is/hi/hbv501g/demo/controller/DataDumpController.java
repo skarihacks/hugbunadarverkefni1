2435f4d16b6193574af7b9e0fb8eb747c96f68a5
skarihacks <skarivinna@gmail.com>
2025-11-05 15:20:22 +0000

    data dump fix

--- src/main/java/is/hi/hbv501g/demo/controller/DataDumpController.java
+++ src/main/java/is/hi/hbv501g/demo/controller/DataDumpController.java
@@ -1,11 +1,9 @@
 package is.hi.hbv501g.demo.controller;
 
-import is.hi.hbv501g.demo.service.AuthService;
 import is.hi.hbv501g.demo.service.DataDumpService;
 import is.hi.hbv501g.demo.service.DataDumpService.DumpResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,16 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DataDumpController {
 
     private final DataDumpService dataDumpService;
-    private final AuthService authService;
 
-    public DataDumpController(DataDumpService dataDumpService, AuthService authService) {
+    public DataDumpController(DataDumpService dataDumpService) {
         this.dataDumpService = dataDumpService;
-        this.authService = authService;
     }
 
     @GetMapping
-    public ResponseEntity<DumpResponse> dump(@RequestHeader(AuthController.SESSION_HEADER) String sessionId) {
-        authService.requireUser(sessionId);
+    public ResponseEntity<DumpResponse> dump() {
         return ResponseEntity.ok(dataDumpService.dumpAll());
     }
 }
